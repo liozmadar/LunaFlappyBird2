32294c30fede1927dc9a04893cf00102f87a4668
liozmadar <liozmadar@gmail.com>
2022-08-29 17:11:40 +0300

    moving scripts

---
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform player;
+    public float smoothing = 5f;
+    public Vector3 offset;
+
+    public Vector2 minPos;
+    public Vector2 maxPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 targetCamPos = player.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+
+        targetCamPos.x = Mathf.Clamp(transform.position.x, minPos.x, maxPos.x);
+        targetCamPos.y = Mathf.Clamp(transform.position.y, minPos.y, maxPos.y);
+    }
+}
